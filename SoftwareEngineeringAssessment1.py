8fa696b28b7d477bac9ee57a0b04ca0727a60833
Fulljames1 <fullyjnr@gmail.com>
2018-11-04 17:11:20 +0000

    Comment update
    Old irrelevant comments were removed in favour of useful comments relating to the code.

--- SoftwareEngineeringAssessment1.py
+++ SoftwareEngineeringAssessment1.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
 
 from selenium import webdriver
 #import time
@@ -11,14 +6,9 @@ br = webdriver.Firefox(executable_path='C:/Users/lewis/Downloads/geckodriver.exe
 br.implicitly_wait(15) # wait's for the page to get done
 #loading before it does anything with it
 
+#Give output to console and expect a input from user
 name = input("Please enter a movie ");
 type(name)
 
-
+#API key with name variable
 br.get('http://www.omdbapi.com/?t='+name+'&apikey=5a902c4d')
-# to fill out a form
-#search = br.find_element_by_name('q')
-#search.send_keys('blade runner')
-#search.submit()
-#time.sleep(5)
-#print(br.title)
